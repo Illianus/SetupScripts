5990f36a52df5418ab7ec28e5f7c0dcc4e567bbd
Dave Roberts <droberts@abacus.systems>
2019-01-16 19:50:49 +0000

    Updated colouring

--- SetupScript.ps1
+++ SetupScript.ps1
@@ -1,12 +1,12 @@
 ﻿function Run-Script {
     Param([String]$script, $baseUri="https://raw.githubusercontent.com/Microsoft/windows-dev-box-setup-scripts/master/scripts/")
-    Write-Host "Installing script from $baseUri$script" -ForegroundColor Yellow
+    Write-Host "Installing script from $baseUri$script" -ForegroundColor Yellow -BackgroundColor Blue
     iex ((New-Object net.webclient).DownloadString("$baseUri$script"))
 }
 
 
 function Add-Chocolatey {
-    Write-Host "Installing Chocolatey" -ForegroundColor Yellow
+    Write-Host "Installing Chocolatey" -ForegroundColor Yellow -BackgroundColor Blue
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
@@ -23,15 +23,15 @@ function Add-Basics {
         "atom",
         "openjdk"
     )|%{ 
-        Write-Host "Installing $_" -ForegroundColor Yellow
+        Write-Host "Installing $_" -ForegroundColor Yellow -BackgroundColor Blue
         choco install $_ -y 
     }
     RefreshEnv
 }
 
 function Add-Python {
-    Write-Host "Installing Python" -ForegroundColor Yellow
-    choco install python
+    Write-Host "Installing Python" -ForegroundColor Yellow -BackgroundColor Blue
+    choco install python -y
     RefreshEnv
     Write-Host "Updating pip" -ForegroundColor Yellow
     python -m pip install --upgrade pip
@@ -40,8 +40,9 @@ function Add-Python {
         pip install $_
     }
     RefreshEnv
-    Write-Host "Installing Visual C++ Redist" -ForegroundColor Yellow
+    Write-Host "Installing Visual C++ Redist" -ForegroundColor Yellow -BackgroundColor Blue
     choco install -y vcredist2015
+    RefreshEnv
 }
 
 function Add-Scripts {
@@ -52,6 +53,7 @@ function Add-Scripts {
         "RemoveDefaultApps.ps1",
         "SystemConfiguration.ps1"
     )|%{Run-Script $_}
+    RefreshEnv
 }
 
 function Setup-System {
