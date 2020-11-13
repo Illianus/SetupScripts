bcd5b9f5ab7a31ebd6fe9f6ac6a439726640df22
Dave Roberts <daveroberts@castletechnicalsolutions.co.uk>
2020-11-13 11:04:02 +0000

    Added NUC

---
+++ IntelNuc.ps1
@@ -0,0 +1,28 @@
+#Requires -RunAsAdministrator
+
+function Set-System {
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $SystemName
+    )
+
+    Get-Volume -DriveLetter C | Set-Volume -NewFileSystemLabel $SystemName
+    Rename-Computer $SystemName
+}
+
+function Format-Media {
+    Get-Disk | ? PartitionStyle -EQ RAW | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Media"
+}
+
+function Setup-NUC {
+    param(
+        [Parameter(Mandatory=true)]
+        [String]
+        $SystemName
+    )
+
+    Set-System -SystemName $SystemName
+    Format-Media
+    Restart-Computer
+}
